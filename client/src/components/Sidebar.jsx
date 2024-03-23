8e4d66598ae2df4bb811f39b74738488bff221cd
shivpratikhande <shivpratikhande2017@gmail.com>
2024-03-23 18:42:07 +0530

    added sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ function Sidebar() {
         <h2 className=' font-bold text-3xl px-5 mb-2 mt-2'>Features</h2>
          <hr />
         <ul  className=' mx-auto'>
-            <li  className=' hover:bg-slate-100  rounded-lg p-5 '>CHAT BOT</li>
+            <li  className=' hover:bg-slate-100  rounded-lg p-5 '>CHAT BOT </li>
             <li  className=' hover:bg-slate-100  rounded-lg p-5 '>DISEASE</li>
             <li  className=' hover:bg-slate-100  rounded-lg p-5 '>LABS</li>
             <li  className=' hover:bg-slate-100  rounded-lg p-5 '>FERTILISER</li>
