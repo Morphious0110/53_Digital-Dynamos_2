de640e50ad369fd8321322b25d4c06aa63ec80de
shivpratikhande <shivpratikhande2017@gmail.com>
2024-03-24 13:09:37 +0530

    added harvestify

---
+++ client/src/components/MIddlePart.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+function MIddlePart() {
+    const Navigate = useNavigate()
+  return (
+    <div className=' bg-slate-200 w-screen rounded-lg p-5 mt-5'>
+
+      hello
+     
+    
+    </div>
+  )
+  
+}
+
+/* function ContentRoutes() {
+  return (
+    <Routes>
+      <Route path='/content/chatbot' element={<Chat />} />
+      <Route path='/content/fertiliser' element={<FertilizerShop />} />
+    </Routes>
+  );
+} */
+
+export default MIddlePart
