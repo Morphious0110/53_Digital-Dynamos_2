1db64be1b22ecce66f21815ec6c9b31915c974ab
Vedant Sanap <vedantsanap123@gmail.com>
2024-03-24 07:48:12 +0530

    New

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Profile from './pages/Profile.jsx'
-import Signup from './pages/Signup.jsx'
-import About from './pages/About.jsx'
-import Signin from './pages/Signin.jsx'
-import Header from './components/Header.jsx'
-import Content from './pages/Content.jsx'
-import ChatBot from './pages/ChatBot.jsx'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import Signup from "./pages/Signup.jsx";
+import About from "./pages/About.jsx";
+import Signin from "./pages/Signin.jsx";
+import Header from "./components/Header.jsx";
+import Content from "./pages/Content.jsx";
+import ChatBot from "./pages/ChatBot.jsx";
+import GenerateText from "./chatbot/Chatbot.jsx";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-      <Header/>
+      <BrowserRouter>
+        <GenerateText />
+        {/* <Header/>
       <Routes>
         <Route path='/ChatBot' element={<ChatBot />}/>
         <Route path='/' element={<Home/>} />
@@ -22,10 +24,10 @@ function App() {
         <Route path='/profile' element={<Profile/>} />
         <Route path='/signin' element={<Signin/>} />
         <Route path='/signup' element={<Signup/>}/>
-      </Routes>
-    </BrowserRouter>
+      </Routes> */}
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
